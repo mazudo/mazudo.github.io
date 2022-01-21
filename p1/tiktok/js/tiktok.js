fbf7077b9c11a2bddc64da984588bc1a5ef1d57d
mazudo <msudo@eastsideprep.org>
2022-01-21 09:11:54 -0800

    final tiktok code

--- p1/tiktok/js/tiktok.js
+++ p1/tiktok/js/tiktok.js
@@ -15,14 +15,31 @@ function magicAlgorithm(numLikes, comment) {
     return rank;
 }
 
-alert("Welcome to Tiktok! Imagine I'm showing you a tiktok video right now");
-let likeCount = Number(prompt("How many likes did this Tiktok video get?"));
-// ask for a video comment
-let comment = prompt("What is the most recent comment for this video?");
+function popup() {
+    alert("Welcome to Tiktok! Imagine I'm showing you a tiktok video right now");
+    let likeCount = Number(prompt("How many likes did this Tiktok video get?"));
+    // ask for a video comment
+    let comment = prompt("What is the most recent comment for this video?");
 
-// using the two inputs above
-// pass that into the magic function and return the result in an alert popup
-let rankResult = magicAlgorithm(likeCount, comment);
-let resultString = "Based off of the magic algorithm, this video's rank is: " + rankResult;
+    // using the two inputs above
+    // pass that into the magic function and return the result in an alert popup
+    let rankResult = magicAlgorithm(likeCount, comment);
+    let resultString = "Based off of the magic algorithm, this video's rank is: " + rankResult;
 
-alert(resultString);
+    alert(resultString);
+}
+
+function showUI() {
+    // grab values from text boxes
+    let likeCount = Number(document.getElementById("like-textbox").value);
+    let comment = document.getElementById("comment-textbox").value;
+    // TODO grab your additional inputs from the html with IDs
+
+    // do the magic
+    // TODO: Pass all 4 inputs into your magic function
+    let rankResult = magicAlgorithm(likeCount, comment);
+
+    // display the output
+    document.getElementById("result-paragraph").innerHTML = "Tiktok Rank: " + rankResult;
+
+}
