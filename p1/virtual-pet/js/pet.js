06962d2f2340954e2674091cc3530cc9dd768dbe
mazudo <msudo@eastsideprep.org>
2022-01-31 10:36:18 -0800

    second meter

--- p1/virtual-pet/js/pet.js
+++ p1/virtual-pet/js/pet.js
@@ -1,4 +1,4 @@
-let mood = 0;
+let mood = 5;
 let belly = 5;
 
 function feedPet() {
@@ -12,20 +12,42 @@ function feedPet() {
     refreshUI();
 }
 
+function petPet() {
+    console.log("Petting pet...");
+    if (mood < 10) {
+        mood += 1;
+    }
+    console.log("mood: " + mood);
+
+    refreshUI();
+}
+
 function decreasePet() {
     // decrease belly value
     if (belly > 0) {
         belly -= 1;
     }
+
+    // decrease mood value
+    if (mood > 0) {
+        mood -= 1;
+    }
+
     refreshUI();
 }
 
 function refreshUI() {
+    // update belly meter + paragraph
     let bellyMeter = document.getElementById("belly-meter");
     bellyMeter.value = belly;
-
     let bellyParagraph = document.getElementById("belly-paragraph");
     bellyParagraph.innerHTML = belly;
+
+    // update mood meter + paragraph
+    let moodMeter = document.getElementById("mood-meter");
+    moodMeter.value = mood;
+    let moodParagraph = document.getElementById("mood-paragraph");
+    moodParagraph.innerHTML = mood;
 }
 
 let petTimer = setInterval(decreasePet, 1000);
