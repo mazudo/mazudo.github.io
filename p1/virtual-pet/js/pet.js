ee86ffd4fc1e3fb05fa2d1f3d347416c88e457e5
mazudo <msudo@eastsideprep.org>
2022-01-31 10:16:40 -0800

    decrease meter with timer

--- p1/virtual-pet/js/pet.js
+++ p1/virtual-pet/js/pet.js
@@ -1,1 +1,31 @@
-console.log("hello Eevee!");
+let mood = 0;
+let belly = 5;
+
+function feedPet() {
+    console.log("Feeding pet...");
+    // belly = belly + 1;
+    if (belly < 10) {
+        belly += 1;
+    }
+    console.log("belly: " + belly);
+
+    refreshUI();
+}
+
+function decreasePet() {
+    // decrease belly value
+    if (belly > 0) {
+        belly -= 1;
+    }
+    refreshUI();
+}
+
+function refreshUI() {
+    let bellyMeter = document.getElementById("belly-meter");
+    bellyMeter.value = belly;
+
+    let bellyParagraph = document.getElementById("belly-paragraph");
+    bellyParagraph.innerHTML = belly;
+}
+
+let petTimer = setInterval(decreasePet, 1000);
